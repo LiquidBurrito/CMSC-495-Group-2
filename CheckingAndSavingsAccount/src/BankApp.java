bc33af8b8168ad6ea848af095a530fd1662c89a3
LiquidBurrito <66260225+LiquidBurrito@users.noreply.github.com>
2022-06-25 13:45:30 -0700

    Updated GUI
    Updated GUI for login and bank accounts. Added GUI elements we had created to the card layout format.

--- CheckingAndSavingsAccount/src/BankApp.java
+++ CheckingAndSavingsAccount/src/BankApp.java
@@ -2,85 +2,109 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Random;
 
 public class BankApp extends JFrame {
-    private JButton btnFrame1;
-    private JButton btnFrame2;
-    private JButton button1;
-    private JTextField thisIsPanel3TextField;
-    private JButton btn2Frame1;
+
     private JPanel cards;
+    ImageIcon icon = new ImageIcon(ClassLoader.getSystemResource("Money.png"));
+    // random number generation for account balance
+    Double min = 100.00;
+    Double max = 10000.00;
+    double randoDouble = (Math.random() * ((max - min) + 1)) + min;
+    double roundDouble = Math.round(randoDouble * 100.0) / 100.0;
+    // random number generation for savings account balance
+    Double min2 = 2500.00;
+    Double max2 = 100000.00;
+    double randoDouble2 = (Math.random() * ((max2 - min2) + 1)) + min2;
+    double roundDouble2 = Math.round(randoDouble2 * 100.0) / 100.0;
+    // random number generation for days since last login
+    private static final Random rando = new Random();
+    private static final int MAX = 730;
+    private static final int MIN = 31;
+    Integer randoInt = rando.nextInt((MAX-MIN)+1)+MIN;
     public BankApp(){
         JFrame frame = new JFrame();
 
         JPanel p1 = loginScreen();
-        p1.setBackground(Color.RED);
+        p1.setBackground(Color.orange);
         JPanel p2 = checkingScreen();
-        p2.setBackground(Color.BLUE);
+        p2.setBackground(Color.orange);
         JPanel p3 = savingsScreen();
-        p3.setBackground(Color.GREEN);
+        p3.setBackground(Color.orange);
 
-
-        //Create the panel that contains the "cards".
+        // create the panel that contains the "cards"
         cards = new JPanel(new CardLayout());
         cards.add(p1, "Panel 1");
         cards.add(p2, "Panel 2");
         cards.add(p3, "Panel 3");
 
-        // Add your card container to the frame
+        // add your card container to the frame
         Container pane = frame.getContentPane();
         pane.add(cards, BorderLayout.CENTER);
 
-        frame.setTitle("Bank of Alaska");
+        frame.setTitle("SJC Credit Union");
+        frame.setIconImage(icon.getImage());
         frame.setLocation(new Point(500,300));
+        frame.setResizable(false);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setSize(400, 500);
+        frame.setSize(800, 400);
         frame.setVisible(true);
     }
-
-
-
+    // loginScreen GUI
     public JPanel loginScreen(){
-        JLabel password1, label;
-        JTextField username;
-        JButton button;
-        JPasswordField Password;
 
         JPanel panel = new JPanel();
         panel.setLayout(null);
 
-        label = new JLabel ("Username");
-        label.setBounds(100,168,70,20);
-        panel.add(label);
+        // JLabels for loginScreen
+        JLabel welcomeLabel = new JLabel("Welcome To SJC Credit Union");
+        welcomeLabel.setBounds(20, 5, 800, 50);
+        welcomeLabel.setFont(new Font("Monaco", Font.BOLD, 50));
+        panel.add(welcomeLabel);
 
-        username = new JTextField();
-        username.setBounds(100,187,193,28);
-        panel.add(username);
+        JLabel userLabel = new JLabel("Username");
+        userLabel.setBounds(290, 100, 100, 20);
+        userLabel.setFont(new Font("Monaco",Font.BOLD, 15));
+        panel.add(userLabel);
 
-        password1 = new JLabel("Password");
-        password1.setBounds(100,215,70,20);
-        panel.add(password1);
+        JLabel passLabel = new JLabel("Password");
+        passLabel.setBounds(290, 160, 100, 20);
+        passLabel.setFont(new Font("Monaco",Font.BOLD, 15));
+        panel.add(passLabel);
 
-        Password = new JPasswordField();
-        Password.setBounds(100,235,193,28);
-        panel.add(Password);
+        // JFields for loginScreen
+        JTextField userText = new JTextField(20);
+        userText.setBounds(290,125,200, 25);
+        panel.add(userText);
 
-        button = new JButton("Login");
-        button.setBounds(100,270,90,25);
-        button.setForeground(Color.WHITE);
-        button.setBackground((Color.BLACK));
-        button.addActionListener(new ActionListener() {
+        JPasswordField passText = new JPasswordField();
+        passText.setBounds(290,185,200,25);
+        panel.add(passText);
+
+        // JButtons for loginScreen
+        JButton loginButton = new JButton("Login");
+        loginButton.setBounds(340,225,90,25);
+        loginButton.setFont(new Font("Monaco", Font.BOLD, 15));
+        loginButton.setForeground(Color.WHITE);
+        loginButton.setBackground((Color.BLACK));
+        panel.add(loginButton);
+
+        // ActionListeners for loginScreen
+        loginButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
                 CardLayout cl = (CardLayout)(cards.getLayout());
                 cl.next(cards);
             }
         });
-        panel.add(button);
+
 
         return panel;
     }
-
+    // checkingScreen GUI
     public JPanel checkingScreen() {
         JPanel panel = new JPanel();
         panel.setLayout(null);
@@ -90,175 +114,226 @@ public class BankApp extends JFrame {
           All components must have the component.setBounds(x position, y position, width, height);
         * */
 
+        // JLabels for checkingScreen
+        Date date = new Date();
+        new SimpleDateFormat();
+        JLabel dTG = new JLabel("Today is " + date);
+        dTG.setBounds(260, 315, 300, 20);
+        dTG.setFont(new Font("Monaco", Font.BOLD, 15));
+        panel.add(dTG);
 
-        JButton button = new JButton("Logout");
-        button.setBounds(60,360,120,25);
-        JButton button2 = new JButton("View Savings");
-        button2.setBounds(200,360,120,25);
-        button.setForeground(Color.WHITE);
-        button.setBackground((Color.BLACK));
-        button2.setForeground(Color.WHITE);
-        button2.setBackground((Color.BLACK));
-        button.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                CardLayout cl = (CardLayout)(cards.getLayout());
-                cl.show(cards, "Panel 1");
+        JLabel lastLog = new JLabel("Your last login was " + randoInt + " days ago");
+        lastLog.setBounds(260, 340, 300, 20);
+        lastLog.setFont(new Font("Monaco", Font.BOLD, 15));
+        panel.add(lastLog);
+
+        JLabel accountLabel = new JLabel("Checking Account");
+        accountLabel.setBounds(20, 5, 800, 60);
+        accountLabel.setFont(new Font("Monaco", Font.BOLD, 50));
+        panel.add(accountLabel);
+
+        JLabel currentBalance = new JLabel("Balance: $"+roundDouble);
+        currentBalance.setBounds(435, 85, 350, 40);
+        currentBalance.setFont(new Font("Monaco", Font.BOLD, 25));
+        panel.add(currentBalance);
+
+        JLabel transactionType = new JLabel("Please choose a transaction type");
+        transactionType.setBounds(20, 90, 250, 20);
+        transactionType.setFont(new Font("Monaco", Font.BOLD, 15));
+        panel.add(transactionType);
+
+        JLabel transactionAmount = new JLabel("Enter your transaction amount");
+        transactionAmount.setBounds(20, 200, 250, 20);
+        transactionAmount.setFont(new Font("Monaco", Font.BOLD, 15));
+        panel.add(transactionAmount);
+
+        // JTextFields for checkingScreen
+        JTextField transactionInput = new JTextField("$");
+        transactionInput.setBounds(20, 230, 230, 40);
+        transactionInput.setFont(new Font("Monaco", Font.BOLD, 15));
+        panel.add(transactionInput);
+
+        // JComboBox for checkingScreen
+        String[] transactions1 = {"Deposit","Withdraw"};
+        JComboBox jCB = new JComboBox(transactions1);
+        jCB.setBounds(20,120,100,20);
+        jCB.setRenderer(new DefaultListCellRenderer() {
+            public void paint(Graphics g) {
+                setBackground(Color.green);
+                setForeground(Color.black);
+                super.paint(g);
             }
         });
-        panel.add(button);
+        panel.add(jCB);
 
-        button2.addActionListener(new ActionListener() {
+        //JButtons for checkingScreen
+        JButton submitButton = new JButton("Submit");
+        submitButton.setBounds(270, 230, 100, 40);
+        submitButton.setFont(new Font("Monaco", Font.BOLD, 15));
+        submitButton.setForeground(Color.WHITE);
+        submitButton.setBackground((Color.BLACK));
+        panel.add(submitButton);
+
+        JButton goToButton = new JButton("Go To Savings");
+        goToButton.setBounds(20,320,130,25);
+        goToButton.setFont(new Font("Monaco", Font.BOLD, 12));
+        goToButton.setForeground(Color.WHITE);
+        goToButton.setBackground((Color.BLACK));
+        panel.add(goToButton);
+
+        JButton logoutButton = new JButton("Logout");
+        logoutButton.setBounds(650,320,120,25);
+        logoutButton.setFont(new Font("Monaco", Font.BOLD, 15));
+        logoutButton.setForeground(Color.WHITE);
+        logoutButton.setBackground((Color.BLACK));
+        panel.add(logoutButton);
+
+        // actionListeners for checkingScreen
+        submitButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+
+        goToButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
                 CardLayout cl = (CardLayout)(cards.getLayout());
                 cl.show(cards, "Panel 3");
             }
         });
-        panel.add(button2);
-        return panel;
-    }
 
-    public JPanel savingsScreen(){
-        JPanel panel = new JPanel();
-        panel.setLayout(null);
+        //return panel;
 
-        /*Enter your GUI code here for the Savings account.
-          Make sure to add components to panel using panel.add(component)
-          All components must have the component.setBounds(x position, y position, width, height);
-        * */
-
-        JButton button = new JButton("Logout");
-        button.setBounds(60,360,120,25);
-        JButton button2 = new JButton("View Checking");
-        button2.setBounds(200,360,120,25);
-        button.setForeground(Color.WHITE);
-        button.setBackground((Color.BLACK));
-        button2.setForeground(Color.WHITE);
-        button2.setBackground((Color.BLACK));
-        button.addActionListener(new ActionListener() {
+        logoutButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
                 CardLayout cl = (CardLayout)(cards.getLayout());
                 cl.show(cards, "Panel 1");
             }
         });
-        panel.add(button);
 
-        button2.addActionListener(new ActionListener() {
+        return panel;
+
+    }
+
+    // savingsScreen GUI
+    public JPanel savingsScreen(){
+        JPanel panel = new JPanel();
+        panel.setLayout(null);
+
+        /*Enter your GUI code here for the checking account.
+          Make sure to add components to panel using panel.add(component)
+          All components must have the component.setBounds(x position, y position, width, height);
+        * */
+
+        // JLabels for savingsScreen
+        Date date = new Date();
+        new SimpleDateFormat();
+        JLabel dTG = new JLabel("Today is " + date);
+        dTG.setBounds(260, 315, 300, 20);
+        dTG.setFont(new Font("Monaco", Font.BOLD, 15));
+        panel.add(dTG);
+
+        JLabel lastLog = new JLabel("Your last login was " + randoInt + " days ago");
+        lastLog.setBounds(260, 340, 300, 20);
+        lastLog.setFont(new Font("Monaco", Font.BOLD, 15));
+        panel.add(lastLog);
+
+        JLabel accountLabel = new JLabel("Savings Account");
+        accountLabel.setBounds(20, 5, 800, 60);
+        accountLabel.setFont(new Font("Monaco", Font.BOLD, 50));
+        panel.add(accountLabel);
+
+        JLabel currentBalance = new JLabel("Balance: $"+roundDouble2);
+        currentBalance.setBounds(435, 85, 350, 40);
+        currentBalance.setFont(new Font("Monaco", Font.BOLD, 25));
+        panel.add(currentBalance);
+
+        JLabel transactionType = new JLabel("Please choose a transaction type");
+        transactionType.setBounds(20, 90, 250, 20);
+        transactionType.setFont(new Font("Monaco", Font.BOLD, 15));
+        panel.add(transactionType);
+
+        JLabel transactionAmount = new JLabel("Enter your transaction amount");
+        transactionAmount.setBounds(20, 200, 250, 20);
+        transactionAmount.setFont(new Font("Monaco", Font.BOLD, 15));
+        panel.add(transactionAmount);
+
+        JLabel interestAccrued = new JLabel("Interest accrued since last login $TBD");
+        interestAccrued.setBounds(435, 120, 350, 20);
+        interestAccrued.setFont(new Font("Monaco", Font.BOLD, 15));
+        panel.add(interestAccrued);
+
+        // JTextFields for savingsScreen
+        JTextField transactionInput = new JTextField("$");
+        transactionInput.setBounds(20, 230, 230, 40);
+        transactionInput.setFont(new Font("Monaco", Font.BOLD, 15));
+        panel.add(transactionInput);
+
+        // JComboBox for savingsScreen
+        String[] transactions1 = {"Deposit","Withdraw"};
+        JComboBox jCB = new JComboBox(transactions1);
+        jCB.setBounds(20,120,100,20);
+        jCB.setRenderer(new DefaultListCellRenderer() {
+            public void paint(Graphics g) {
+                setBackground(Color.green);
+                setForeground(Color.black);
+                super.paint(g);
+            }
+        });
+        panel.add(jCB);
+
+        //JButtons for savingsScreen
+        JButton submitButton = new JButton("Submit");
+        submitButton.setBounds(270, 230, 100, 40);
+        submitButton.setFont(new Font("Monaco", Font.BOLD, 15));
+        submitButton.setForeground(Color.WHITE);
+        submitButton.setBackground((Color.BLACK));
+        panel.add(submitButton);
+
+        JButton goToButton = new JButton("Go To Checking");
+        goToButton.setBounds(20,320,130,25);
+        goToButton.setFont(new Font("Monaco", Font.BOLD, 12));
+        goToButton.setForeground(Color.WHITE);
+        goToButton.setBackground((Color.BLACK));
+        panel.add(goToButton);
+
+        JButton logoutButton = new JButton("Logout");
+        logoutButton.setBounds(650,320,120,25);
+        logoutButton.setFont(new Font("Monaco", Font.BOLD, 15));
+        logoutButton.setForeground(Color.WHITE);
+        logoutButton.setBackground((Color.BLACK));
+        panel.add(logoutButton);
+
+        // actionListeners for savingsScreen
+        submitButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+
+        goToButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
                 CardLayout cl = (CardLayout)(cards.getLayout());
                 cl.show(cards, "Panel 2");
             }
         });
-        panel.add(button2);
+
+        //return panel;
+
+        logoutButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                CardLayout cl = (CardLayout)(cards.getLayout());
+                cl.show(cards, "Panel 1");
+            }
+        });
+
         return panel;
+
     }
 }
-
-
-
-
-
-
-
-
-
-    //Below this is commented out code from previous build
-    /*
-//    LoginGui loginGui = new LoginGui();
-    Checking checking = new Checking();
-    Savings savings = new Savings();
-
-    JPanel bankPanel = new JPanel(new GridLayout(4, 1, 0, 20));
-    JPanel panel1 = new JPanel(new GridLayout(1, 2, 10, 0));
-    JPanel panel2 = new JPanel(new GridLayout(1, 2, 10, 0));
-
-    JLabel label1 = new JLabel("Label 1");
-    JLabel label2 = new JLabel("Label 2");
-    JButton button1 = new JButton("Button 1");
-    JButton button2 = new JButton("Button 2");
-    JButton logoutButton = new JButton("Logout");
-    String comboBoxItems[] = {"Checking", "Savings"};
-    JComboBox jComboBox = new JComboBox(comboBoxItems);
-
-    public static void createAndShowGUI() {
-        JFrame frame = new JFrame("Bank Application");
-        frame.setLayout(new FlowLayout());
-        frame.setSize(300, 250);
-        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-        BankApp app = new BankApp();
-        app.addComponentToPane(frame.getContentPane());
-
-        LoginGui loginGui = new LoginGui();
-
-        frame.setVisible(true);
-    }
-
-    public void addComponentToPane(Container pane) {
-        panel1.add(label1);
-        panel1.add(button1);
-        panel2.add(label2);
-        panel2.add(button2);
-
-        bankPanel.add(panel1);
-        bankPanel.add(panel2);
-        bankPanel.add(jComboBox);
-        bankPanel.add(logoutButton);
-
-        pane.add(bankPanel);
-    }
-
-    public void test(){
-        //this is a test from Sergio
-    }
-
-    public void caseyTest() {
-        // this is a test from Casey O....
-    }
-
-    public BankApp() {
-
-    }
-
-
-}
-
-class LoginGui {
-    JFrame frame = new JFrame("Login");
-
-    JLabel usernameLabel = new JLabel("Username");
-    JTextField usernameTextField = new JTextField("Enter Username", 10);
-    JButton loginButton = new JButton("Login");
-
-    JPanel loginPanel = new JPanel(new GridLayout(4, 1));
-    JPanel usernamePanel = new JPanel(new GridLayout(1, 2));
-
-    JPanel bankPanel = new JPanel(new GridLayout(4, 1, 0, 10));
-    JPanel panel1 = new JPanel(new GridLayout(1, 2, 10, 0));
-    JPanel panel2 = new JPanel(new GridLayout(1, 2, 10, 0));
-
-    public LoginGui() {
-        frame.setSize(240, 150);
-        frame.setLayout(new FlowLayout());
-        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-        addComponentToPane(frame.getContentPane());
-        frame.pack();
-        frame.setVisible(true);
-
-    }
-
-    public void addComponentToPane(Container pane) {
-        usernamePanel.add(usernameLabel);
-        usernamePanel.add(usernameTextField);
-
-        loginPanel.add(usernamePanel);
-        loginPanel.add(new JLabel(""));
-        loginPanel.add(new JLabel(""));
-        loginPanel.add(loginButton);
-
-        pane.add(loginPanel);
-    }
-*/
-
