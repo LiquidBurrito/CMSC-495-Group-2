cf94761eb559ee84c2a485630662a61eb186b797
sarispeg <94018170+sarispeg@users.noreply.github.com>
2022-06-30 19:25:33 -0400

    S

--- CheckingAndSavingsAccount/src/BankAccount.java
+++ CheckingAndSavingsAccount/src/BankAccount.java
@@ -1,5 +1,78 @@
+import java.util.*;
 public class BankAccount {
 
+    private Map<String, Object>account = new HashMap<>();
+    private double checkingBalance = 0;
+    private double savingsBalance = 0;
+    private int dayz = 0;
+
+    public BankAccount(){
+        account.put("Username", "admin");
+        account.put("Password", "password");
+        account.put("Checking", checkingBalance);
+        account.put("Savings", savingsBalance);
+        account.put("Days", dayz);
+    }
+
+    public boolean checkUser(String usrnm){
+        String s = (String) account.get("Username");
+        return s.equalsIgnoreCase(usrnm);
+    }
+
+    public boolean checkPass(String pass){
+        return account.get("Password").equals(pass);
+    }
+
+
+    public double getChecking (){
+        return checkingBalance;
+    }
+
+    public double getSavings (){
+        return savingsBalance;
+    }
+
+    public void depositCheck (Double value){
+        checkingBalance = checkingBalance + value;
+    }
+
+    public void depositSaving (Double value){
+        savingsBalance = savingsBalance + value;
+    }
+
+    public void withdrawChecking(Double value) {
+        if (checkingBalance - value < 0)
+            //Throw exception;
+            checkingBalance = checkingBalance - value;
+
+    }
+
+    public void withdrawSaving(Double value) {
+        if (savingsBalance - value < 0)
+            //Throw exception;
+            savingsBalance = savingsBalance - value;
+
+    }
+
+    public int getDays(){
+        return (int) account.get("Days");
+    }
+
+
+
+    public void calcInterest() {
+        int d = (int) account.get("Days");
+        if (d > 30) {
+            //this.deposit(interestAccrued);
+        }
+    }
+    public double interestAccrued() {
+        return savingsBalance = savingsBalance * 0.015;
+    }
+
+
+
+    /*
     int balance;
 
     public BankAccount(int balance) {
@@ -23,4 +96,6 @@ public class BankAccount {
         return super.toString();
 
     }
+
+     */
 }
